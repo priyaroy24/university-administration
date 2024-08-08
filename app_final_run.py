064cd64923625631ca4bf05e3b05a753927dd1e4
priyaroy24 <priya240612@gmail.com>
2024-08-08 10:34:15 +0530

    Update app_final_run.py

--- app_final_run.py
+++ app_final_run.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-clf = pickle.load(open("case_study_university.pkl","rb"))
+clf = pickle.load(open("case_study_university.pkl","wb"))
 
 def predict(data):
     clf = pickle.load(open("case_study_university.pkl","rb"))
